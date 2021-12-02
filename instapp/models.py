379afacfcc13f2ace7fd0feeb8a332e168956db5
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-12-02 18:43:56 +0300

    create image model

--- instapp/models.py
+++ instapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+import datetime as dt
 
 # Create your models here.
+class Images(models.Model):
+    # title field
+    image = CloudinaryField('image')
+    title = models.CharField(max_length=50)
+    caption = models.TextField(max_length=1500)
+    post_date = models.DateTimeField(auto_now_add=True,null=True)
+
+
+    def __str__(self):
+        return self.title
+
+    
