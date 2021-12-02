93e5dcb348bf1d79c67a1994c479c6fe5dc17dfe
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-12-02 20:02:47 +0300

    style homepage

--- instapp/views.py
+++ instapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from instapp.models import Images
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'all-photos/index.html')
+    image = Images.objects.all().order_by('-id')
+
+    return render(request, 'all-photos/index.html',{'image': image})
